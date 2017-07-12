3e1918068ae109c922343dc2acf536162aabe90d
steven hebert <shebert2@cnm.edu>
2017-07-12 14:00:13 -0600

    added base html

--- index.php
+++ index.php
@@ -4,7 +4,44 @@
 		<meta charset="utf-8"/>
 		<title>HMTL Sandy</title>
 	</head>
+	<!-- want to create a table with cats and dogs -->
+	<!-- need to lay out bread before putting peanut butter and jelly together -->
 	<body>
 		<h1>Hello world?</h1>
+		<table>
+			<tr>
+				<th>gato</th>
+				<th>pero</th>
+			</tr>
+			<tr>
+				<td>persian</td>
+				<td>beagle</td>
+			<tr>
+				<td>maine coon</td>
+				<td>rottweiler</td>
+			</tr>
+			<tr>
+				<td>siamese</td>
+				<td>german shepard</td>
+			</tr>
+		</table>
+
+		<!-- lets make a list -->
+		<h2>a few of my favorite things</h2>
+		<ul>
+			<li>tacos</li>
+			<li>the beach</li>
+			<li>kittens</li>
+			<li>fast cars</li>
+		</ul>
+
+		<! --now a numbered list -->
+		<h2>ordianal tasks</h2>
+		<ol>
+			<li>feed kitty</li>
+			<li>sign in for attendence</li>
+			<li>water the plants</li>
+			<li>read textbooks</li>
+		</ol>
 	</body>
 </html>
